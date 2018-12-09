d1072fe3bd594e50ed96cf31214ef4b4b819df48
Kanogaelias <kanogae@gmail.com>
2018-12-09 11:54:42 +0300

    add image location

--- photos/urls.py
+++ photos/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search', views.search_image, name='search_image'),
-    # url(r'^image/(\d+)',views.image,name ='image'),
-    # url(r'location/(\d+)',views.filter_by_location,name ='location'),
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'location/(\d+)',views.filter_by_location,name ='location'),
     # url(r'category/(\d+)',views.filter_by_category,name ='category'),
 ]
 if settings.DEBUG:
